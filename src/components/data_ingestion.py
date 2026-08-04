ad63c0fe43e327bcf9123019d4089ee8efdf3869
anuroy15052005 <anuroy15052005@gmail.com>
2026-08-04 10:19:18 +0530

    Hyperparameter Tunning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,11 @@
 import os
 import sys
+from pathlib import Path
+
+PACKAGE_ROOT = Path(__file__).resolve().parents[2]
+if str(PACKAGE_ROOT) not in sys.path:
+    sys.path.insert(0, str(PACKAGE_ROOT))
+
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,24 +21,27 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values()) [i]
+        for model_name, model in models.items():
+            param_grid = param.get(model_name, {})
 
-            model. fit(X_train, y_train)
+            if param_grid:
+                gs = GridSearchCV(model, param_grid, cv=3)
+                gs.fit(X_train, y_train)
+                model = gs.best_estimator_
+            else:
+                model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys()) [i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
 
